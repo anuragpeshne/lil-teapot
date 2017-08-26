472a609759c09e6783486d5c96f466fca9755ddd
anuragpeshne <anurag.peshne@gmail.com>
2017-08-26 17:35:58 -0400

    pass stdin to programs

---
+++ tests/2-copy-that.py
@@ -0,0 +1,18 @@
+def get_statement():
+    statement = """
+    Challenge 2: Copy that
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    Next, you have to read a string from standard in and echo it back.
+    This will enable us to do interesting things in next challenges.
+    """
+    return statement
+
+def get_expected_output():
+    return get_stdin_str()
+
+def get_stdin_str():
+    return "...Short and stout\n"
+
+def get_input_file_name():
+    return "2-copy-that.${ext}"
